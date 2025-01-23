4a80e2c83c7fe225beeb02274da9ce232ec113e5
daktic <gberg@dakticdigital.com>
2025-01-22 21:41:44 -0500

    Working on adding a test. a bit confused about the signer vs anchor provider, will need to look more into the test framework from anchor

--- tests/digital-nomad-exchange.ts
+++ tests/digital-nomad-exchange.ts
@@ -1,15 +1,51 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
+import { Keypair } from "@solana/web3.js";
 import { DigitalNomadExchange } from "../target/types/digital_nomad_exchange";
+import { createMint, TOKEN_PROGRAM_ID } from "@solana/spl-token";
+import * as bs58 from "bs58";
 
 describe("digital-nomad-exchange", () => {
   // Configure the client to use the local cluster.
   anchor.setProvider(anchor.AnchorProvider.env());
 
   const program = anchor.workspace.DigitalNomadExchange as Program<DigitalNomadExchange>;
+  const provider = anchor.AnchorProvider.env();
+  const connection = provider.connection;
+  const signer = Keypair.fromSecretKey(
+      bs58.decode(
+          "588FU4PktJWfGfxtzpAAXywSNt74AvtroVzGfKkVN1LwRuvHwKGr851uH8czM5qm4iqLbs1kKoMKtMJG4ATR7Ld2"
+      )
+  );
 
-  it("Is initialized!", async () => {
-    // Add your test here.
+  let mintA: anchor.web3.PublicKey;
+  let mintB: anchor.web3.PublicKey;
+  let userTokenAccountA: anchor.web3.PublicKey;
+  let userTokenAccountB: anchor.web3.PublicKey;
+
+  before(async () => {
+    // Create mint A
+    mintA = await createMint(
+        connection,
+        signer,
+        signer.publicKey,
+        null,
+        9
+    );
+
+    // Create mint B
+    mintB = await createMint(
+        connection,
+        signer,
+        signer.publicKey,
+        null,
+        9,
+    );
+  });
+
+
+
+  it("Is initialized with two tokens!", async () => {
     const tx = await program.methods.initialize().rpc();
     console.log("Your transaction signature", tx);
   });
