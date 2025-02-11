f781aae7db99aa0bbbfe5df89cb171a3c4f9796c
daktic <gberg@dakticdigital.com>
2025-02-10 23:28:48 -0500

    Looking at some refrences it seems like I need a PDA to intalize the poolm but I am having a hard time wrapping my head around using the Instructions to call the pool intalization call

--- programs/DNE-factory/src/lib.rs
+++ programs/DNE-factory/src/lib.rs
@@ -1,21 +1,38 @@
 use anchor_lang::prelude::*;
-use digital_nomad_exchange::LiquidityPool;
+use anchor_lang::solana_program::program::invoke_signed;
+use anchor_lang::solana_program::instruction::Instruction;
 
+// Our Factory ID
 declare_id!("F4GTZaky3u6tNQBmYEEzPbpSCVPaKc3ZYjzVpM8NEx3Y");
 
 #[program]
 pub mod dne_factory {
     use super::*;
-    pub fn initialize(ctx: Context<CreateFactory>) -> Result<()> {
+    pub fn initialize_pool(ctx: Context<InitializePool>, bump: u8) -> Result<()> {
+        let pool = &mut ctx.accounts.pool;
+        pool.owner = *ctx.accounts.owner.key;
+        pool.bump = bump;
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct CreateFactory<'info> {
-    #[account(init, payer = user, space = 8 + 32 + 8)]
-    pub factory: Account<'info, LiquidityPool>,
+pub struct InitializePool<'info> {
+    #[account(
+        init,
+        payer = owner,
+        space = 8 + 16,
+        seeds = [b"liquidity_pool", owner.key().as_ref()],
+        bump
+    )]
+    pub pool: Account<'info, PoolState>,
     #[account(mut)]
-    pub user: Signer<'info>,
+    pub owner: Signer<'info>,
     pub system_program: Program<'info, System>,
+}
+
+#[account]
+pub struct PoolState {
+    pub owner: Pubkey,
+    pub bump: u8,
 }
