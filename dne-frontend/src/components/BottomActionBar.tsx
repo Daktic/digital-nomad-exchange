509d44ee4f768a4d4560290b20447da4b1f915d0
daktic <gberg@dakticdigital.com>
2025-02-21 17:13:38 -0500

    Able to add svgs through React compontnes. pretty cool. will need to convert them

--- dne-frontend/src/components/BottomActionBar.tsx
+++ dne-frontend/src/components/BottomActionBar.tsx
@@ -1,13 +1,19 @@
 import {Link} from "wouter";
 import styles from "./BottomActionBar.module.css";
+import {PortfolioIcon} from "../assets/button_bar/Portfolio.tsx";
 
 const BottomNavbar = () => (
     <div className={styles.navbar}>
-        <Link to="/portfolio" className={styles.item}>Portfolio</Link>
-        <Link to="/wallet" className={styles.item}>Wallet</Link>
+        <Link to="/portfolio" className={styles.item}>
+                <PortfolioIcon />
+        </Link>
+        <Link to="/swap" className={styles.item}>Swap</Link>
         <Link to="/pools" className={styles.item}>Settings</Link>
         <Link to="/liquidity" className={styles.item}>Liquidity</Link>
-        <Link to="/swap" className={styles.item}>Swap</Link>
+        <Link to="/wallet" className={styles.item}>Wallet</Link>
+
+
+
     </div>
 );
 
