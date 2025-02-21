fb14b83fb04998403b413698c39beff131c28a5d
daktic <gberg@dakticdigital.com>
2025-02-21 16:59:38 -0500

    Navbar has a little style and is now at the bottom. Need to make these icons

--- dne-frontend/src/components/BottomActionBar.tsx
+++ dne-frontend/src/components/BottomActionBar.tsx
@@ -1,12 +1,13 @@
 import {Link} from "wouter";
+import styles from "./BottomActionBar.module.css";
 
 const BottomNavbar = () => (
-    <div className="navbar">
-    <Link to="/portfolio">Portfolio</Link>
-        <Link to="/wallet">Wallet</Link>
-    <Link to="/pools">Settings</Link>
-    <Link to="/liquidity">Liquidity</Link>
-    <Link to="/swap">Swap</Link>
+    <div className={styles.navbar}>
+        <Link to="/portfolio" className={styles.item}>Portfolio</Link>
+        <Link to="/wallet" className={styles.item}>Wallet</Link>
+        <Link to="/pools" className={styles.item}>Settings</Link>
+        <Link to="/liquidity" className={styles.item}>Liquidity</Link>
+        <Link to="/swap" className={styles.item}>Swap</Link>
     </div>
 );
 
