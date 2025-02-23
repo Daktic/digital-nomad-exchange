4bf7ab01865579ed56de584d34f222365dc85f45
daktic <gberg@dakticdigital.com>
2025-02-22 19:43:20 -0500

    Got some initial structuring together on my portfolio page. As usual, struggling with CSS.

--- dne-frontend/src/components/assetCard/AssetCard.tsx
+++ dne-frontend/src/components/assetCard/AssetCard.tsx
@@ -1,17 +1,60 @@
 import styles from "./AssetCard.module.css";
-import {Asset} from "../../ty"
+import {Asset} from "../../types"
 
-const AssetCard = () => {
+// Prop Types
+interface AssetCardProps {
+    asset: Asset;
+}
+interface AssetImgProps {
+    symbol: string;
+    img_uri: string;
+}
+
+interface AssetSymbolProps {
+    symbol: string;
+}
+
+interface AssetNameProps {
+    asset_name: string
+}
+
+interface AssetPriceProps {
+    price: number;
+}
+
+
+const AssetCard = ({asset}: AssetCardProps) => {
     return (
         <div className={styles.card}>
-            card
+            <AssetImg symbol={asset.symbol} img_uri={asset.token_img} />
+            <AssetSymbol symbol={asset.symbol} />
+            <AssetName asset_name={asset.name} />
+            <AssetPrice price={asset.price} />
         </div>
     )
 }
 
-const AssetImg = (asset:Asset) => (
+const AssetImg = ({symbol, img_uri}: AssetImgProps) => (
     <div>
-        <img alt={}/>
+        <img alt={symbol} src={img_uri}/>
+    </div>
+)
+
+const AssetSymbol = ({symbol}: AssetSymbolProps) => (
+    <div>
+        <p>{symbol}</p>
+    </div>
+)
+
+const AssetName = ({asset_name}: AssetNameProps) => (
+    <div>
+        <p>{asset_name}</p>
+    </div>
+)
+
+const AssetPrice = ({price}: AssetPriceProps) => (
+    <div>
+        <p>{price}</p>
     </div>
 )
 
--- dne-frontend/src/pages/Portfolio.tsx
+++ dne-frontend/src/pages/Portfolio.tsx
@@ -1,12 +1,17 @@
 import {AssetCard} from "../components/assetCard/AssetCard.tsx";
 import styles from "./Pages.module.css";
 
+import {mockWallet} from "../../../mock_data/mock_data.ts";
+
+
+const mock_asset = mockWallet.assets[0]
+
 const Portfolio = () => {
 
     return (
         <div className={styles.page}>
             <h1>Portfolio</h1>
-            <AssetCard />
+            <AssetCard asset={mock_asset} />
         </div>
     )
 }
