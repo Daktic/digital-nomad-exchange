7ae6fdbae8c0c3538ca8800c083cc82d522818b8
Daktic <gberg@DakticDigital.com>
2025-03-01 18:51:07 -0500

    Added something of a placeholder for the chart to sit in later.

---
+++ dne-frontend/src/components/portfolioChart/PortfolioChart.tsx
@@ -0,0 +1,11 @@
+import styles from "./PortfolioChart.module.css";
+
+const PortfolioChart = () => {
+    return (
+        <div className={styles.chartContainer}>
+            chart to come
+        </div>
+    )
+}
+
+export default PortfolioChart;
