1a0a1a2af272814e62632b3d9011d9ea14d57f57
Daktic <gberg@DakticDigital.com>
2025-03-01 20:08:32 -0500

    Adding mock pools to pool

--- dne-frontend/src/pages/Pools.tsx
+++ dne-frontend/src/pages/Pools.tsx
@@ -1,14 +1,16 @@
 import styles from "./Pages.module.css";
 import {mockPools} from "../../../mock_data/mock_data.ts";
+import {PoolCard} from "../components/poolCard/PoolCard.tsx";
 
 const Pools = () => {
 
     return (
         <div className={styles.page}>
-            <h1>Portfolio</h1>
+            <h1>Pools</h1>
             {/*This will later return fetched assets*/}
             {mockPools.map((pool, index) => (
             //     Pool Card
+                <PoolCard key={index} pool={pool}/>
             ))}
         </div>
     )
