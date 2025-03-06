607ecb5184be5adadd34bdcad2b361b8100e5d3b
daktic <gberg@dakticdigital.com>
2025-03-05 20:52:50 -0500

    This doesnt quiet work

--- dne-frontend/src/components/swapBar/SwapBar.tsx
+++ dne-frontend/src/components/swapBar/SwapBar.tsx
@@ -1,6 +1,6 @@
 import styles from "./SwapBar.module.css";
-import { Asset } from "../../types.ts";
-import { useState } from "preact/hooks";
+import {Asset} from "../../types.ts";
+import {useEffect, useState} from "preact/hooks";
 
 interface SwapBarProps {
     tokenA: Asset;
@@ -18,10 +18,125 @@ interface TokenAmountProps {
     update: (amount: number) => void;
 }
 
+interface calculateSwap {
+    tokenAAmount: number;
+    tokenBAmount: number;
+    swapAmount: number;
+    fee:number;
+    lpAmount: number;
+    swapType:swapType
+}
+
+enum swapType {
+    AforB=0,
+    BforA=1,
+    LPforAB=2,
+    ABforLP=3
+}
+
+interface swapProduct {
+    tokenAAmount: number;
+    tokenBAmount: number;
+    lpAmount: number;
+}
+
+const calculateTokenAmounts = (swap: calculateSwap): swapProduct => {
+    let newTokenA: number;
+    let newTokenB: number;
+    let newLp: number;
+
+    console.log(swap)
+
+    if (swap.swapType === 0) {
+        newTokenA = swap.tokenAAmount - swap.swapAmount;
+        newTokenB =  calulateSwap(swap.tokenAAmount, swap.tokenBAmount, swap.swapAmount, swap.fee);
+        newLp = swap.lpAmount;
+    } else if (swap.swapType === 1) {
+        newTokenA = swap.tokenBAmount - swap.swapAmount;
+        newTokenB =  calulateSwap(swap.tokenBAmount, swap.tokenAAmount, swap.swapAmount, swap.fee);
+        newLp = swap.lpAmount;
+    } else {
+        return calulateAddRemove(swap)
+    }
+
+    return {
+        tokenAAmount: newTokenA,
+        tokenBAmount: newTokenB,
+        lpAmount: newLp,
+    }
+};
+
+const calulateSwap = (tokenA:number, tokenB:number, swapAmount:number, fee:number): number => {
+    if (swapAmount <= 0 || tokenA <= 0 || tokenB <= 0) return 0;
+    
+    const amountAfterFee = swapAmount * (1 - fee);
+
+    return (amountAfterFee * tokenB) /
+        (tokenA + amountAfterFee);
+}
+
+const calulateAddRemove = (swap: calculateSwap): swapProduct => {
+    let newTokenA: number;
+    let newTokenB: number;
+    let newLp: number;
+
+    if (swap.swapType === 2) {
+        // LP for AB
+        newTokenA = 0;
+        newTokenB =  0;
+        newLp = 0;
+    } else {
+        // AB for LP
+        newTokenA = 0;
+        newTokenB =  0;
+        newLp = 0;
+    }
+    return {
+        tokenAAmount: newTokenA,
+        tokenBAmount: newTokenB,
+        lpAmount: newLp,
+    }
+
+}
+
+const fee = 0.25;
+
 const SwapBar = ({ tokenA, tokenB }: SwapBarProps) => {
-    const [tokenAAmount, setTokenAAmount] = useState(0);
-    const [tokenBAmount, setTokenBAmount] = useState(0);
-    const [lpTokenAmount, setLPTokenAmount] = useState(0);
+    const [tokenAAmount, setTokenAAmount] = useState(1000);
+    const [tokenBAmount, setTokenBAmount] = useState(1000);
+    const [lpTokenAmount, setLPTokenAmount] = useState(Math.sqrt(1000 * 1000));
+
+    useEffect(() => {
+        // If Token A changes, update others
+        const newState = calculateTokenAmounts({
+            tokenAAmount: tokenAAmount,
+            tokenBAmount: tokenBAmount,
+            swapAmount: tokenAAmount,
+            fee:fee,
+            lpAmount: lpTokenAmount,
+            swapType:swapType.AforB
+        })
+
+        setTokenAAmount(newState.tokenAAmount);
+        setTokenBAmount(newState.tokenBAmount);
+        setLPTokenAmount(newState.lpAmount);
+    }, [tokenAAmount]);
+
+    useEffect(() => {
+        // If Token A changes, update others
+        const newState = calculateTokenAmounts({
+            tokenAAmount: tokenAAmount,
+            tokenBAmount: tokenBAmount,
+            swapAmount: tokenBAmount,
+            fee:fee,
+            lpAmount: lpTokenAmount,
+            swapType:swapType.BforA
+        })
+
+        setTokenAAmount(newState.tokenAAmount);
+        setTokenBAmount(newState.tokenBAmount);
+        setLPTokenAmount(newState.lpAmount);
+    }, [tokenBAmount]);
 
     return (
         <div className={styles.swapBarContainer}>
@@ -60,7 +175,12 @@ update: (amount: number) => void
                        inputMode="decimal"
                        pattern="[0-9.]*"
                        value={amount}
-                       onInput={(e) => update(parseFloat(e.target.value) || 0)}
+                       onInput={(e) => {
+                           const parsedValue = parseFloat(e.target.value);
+                           if (!isNaN(parsedValue)) {
+                               update(parsedValue);
+                           }
+                       }}
     />
 </div>
 );
