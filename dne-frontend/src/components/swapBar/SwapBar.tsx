c8dbd4de10a23df84b036aca47da61accad16f99
daktic <gberg@dakticdigital.com>
2025-03-05 19:53:33 -0500

    Visual aspect of token swap is looking okay for the time being.

--- dne-frontend/src/components/swapBar/SwapBar.tsx
+++ dne-frontend/src/components/swapBar/SwapBar.tsx
@@ -1,5 +1,6 @@
 import styles from "./SwapBar.module.css";
-import {Asset} from "../../types.ts";
+import { Asset } from "../../types.ts";
+import { useState } from "preact/hooks";
 
 interface SwapBarProps {
     tokenA: Asset;
@@ -11,17 +12,35 @@ interface PoolAssetImgProps {
     tokenB: Asset;
 }
 
-const SwapBar = ({tokenA, tokenB}: SwapBarProps) => {
+interface TokenAmountProps {
+    token: Asset;
+    tokenAmount: number;
+    update: (amount: number) => void;
+}
+
+const SwapBar = ({ tokenA, tokenB }: SwapBarProps) => {
+    const [tokenAAmount, setTokenAAmount] = useState(0);
+    const [tokenBAmount, setTokenBAmount] = useState(0);
+    const [lpTokenAmount, setLPTokenAmount] = useState(0);
 
     return (
         <div className={styles.swapBarContainer}>
-            <SwapAddSwitch />
+            <SwapAddSwitch></SwapAddSwitch>
             <div className={styles.swapBar}>
-            <TokenImgs tokenA={tokenA} tokenB={tokenB} />
+                <TokenAmount
+                    token={tokenA}
+                    tokenAmount={tokenAAmount}
+                    update={setTokenAAmount}
+                ></TokenAmount>
+                <TokenAmount
+                    token={tokenB}
+                    tokenAmount={tokenBAmount}
+                    update={setTokenBAmount}
+                ></TokenAmount>
             </div>
         </div>
-    )
-}
+    );
+};
 
 export default SwapBar;
 
@@ -29,15 +48,33 @@ const SwapAddSwitch = () => (
     <div className={styles.swapSwitch}>
 
     </div>
-)
+);
 
-const TokenImgs = ({tokenA, tokenB}: PoolAssetImgProps) => (
-    <div className={styles.poolImageContainer}>
-        <div className={styles.assetImgContainer}>
-            <img className={styles.assetImg} alt={tokenA.name} src={tokenA.token_img}/>
-        </div>
-        <div className={styles.assetImgContainer}>
-            <img className={styles.assetImg} alt={tokenA.name} src={tokenB.token_img}/>
-        </div>
-    </div>
-)
+
+const InputAmount = ({ amount, update }: {
+amount: number;
+update: (amount: number) => void
+}) => (
+<div className={styles.InputAmount}>
+    <input             type="number"
+                       inputMode="decimal"
+                       pattern="[0-9.]*"
+                       value={amount}
+                       onInput={(e) => update(parseFloat(e.target.value) || 0)}
+    />
+</div>
+);
+
+const TokenAmount = ({ token, tokenAmount, update }: TokenAmountProps) => (
+<div className={styles.assetImgContainer}>
+    <img
+        className={styles.assetImg}
+        alt={token.name}
+        src={token.token_img}
+    />
+        <InputAmount
+            amount={tokenAmount}
+            update={update}
+        ></InputAmount>
+</div>
+);
