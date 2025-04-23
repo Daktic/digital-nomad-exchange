323bd060c809bc6762952426012aff9e756750ca
Daktic <gberg@DakticDigital.com>
2025-04-23 15:58:31 -0400

    Updated Home screen

--- dne-frontend/src/app.tsx
+++ dne-frontend/src/app.tsx
@@ -1,9 +1,7 @@
 // src/App.tsx
 import { Router, Route } from 'wouter';
 import Portfolio from './pages/Portfolio';
-import Wallet from './pages/Wallet.tsx';
 import Pools from './pages/Pools';
-import Liquidity from "./pages/Liquidity.tsx";
 import Swap from "./pages/SwapScreen.tsx";
 import Layout from "./Layout.tsx";
 
@@ -41,10 +39,7 @@ const App = () => {
                 <Layout>
                     <Router>
                         <Route path="/" component={Portfolio}/>
-                        <Route path="/portfolio" component={Portfolio}/>
-                        <Route path="/wallet" component={Wallet}/>
                         <Route path="/pools" component={Pools}/>
-                        <Route path="/liquidity" component={Liquidity}/>
                         <Route path="/swap" component={Swap}/>
                     </Router>
                 </Layout>
